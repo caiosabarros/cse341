97e50a9c7c4639b6c5737dca3108f5fbce4f161a
caiohenriquesa1@gmail.com <caiohenriquesa1@gmail.com>
2025-03-12 21:49:31 -0300

    add swagger api-docs

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 
+router.use('/', require('./swagger'));
+
+router.get('/', (req, res) => {
+    res.send('Hello!');
+})
+
 router.use('/contacts', require('./contacts.js'))
 
 module.exports = router;
